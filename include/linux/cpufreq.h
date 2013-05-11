2645961c49c4f642f6453fe9b10beae1e3359c62
shumash <admin@shumash@Ubuntux64.(none)>
2013-05-11 13:45:54 -0600

    fix badass & lagfree governor name def's

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -359,10 +359,10 @@ extern struct cpufreq_governor cpufreq_gov_ondemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemand)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BADASS)
 extern struct cpufreq_governor cpufreq_gov_badass;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_badass)
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_bds)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
 extern struct cpufreq_governor cpufreq_gov_lagfree;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lagfree)
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_dbs)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVE)
 extern struct cpufreq_governor cpufreq_gov_conservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative)
