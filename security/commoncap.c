4337b782e7f6c002dd0c882f0cdd80470df4da3d
James Sullins <jcsullins@gmail.com>
2013-11-21 10:01:19 -0600

    security/commoncap: fix perms for bluetooth/rfkill
    Change-Id: I9a8de2cf70855d6ca68195eba894c2af8eaa3e04

--- security/commoncap.c
+++ security/commoncap.c
@@ -88,9 +88,13 @@ int cap_capable(struct task_struct *tsk, const struct cred *cred, int cap,
 		int audit)
 {
 #ifdef CONFIG_ANDROID_PARANOID_NETWORK
-	if (cap == CAP_NET_RAW && in_egroup_p(AID_NET_RAW))
+	if (cap == CAP_NET_RAW &&
+			(in_egroup_p(AID_NET_RAW) || in_egroup_p(AID_NET_BT))
+			)
 		return 0;
-	if (cap == CAP_NET_ADMIN && in_egroup_p(AID_NET_ADMIN))
+	if (cap == CAP_NET_ADMIN &&
+			(in_egroup_p(AID_NET_ADMIN) || in_egroup_p(AID_NET_BT_ADMIN))
+			)
 		return 0;
 #endif
 	return cap_raised(cred->cap_effective, cap) ? 0 : -EPERM;
